9c42504cb326184e80b4410f22c4d63f16949282
leo_feola <feola.leopoldo@gmail.com>
2022-10-21 17:04:31 +0200

    test accordion

--- assets/global.js
+++ assets/global.js
@@ -1174,10 +1174,10 @@ for (let i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function () {
     this.classList.toggle("active");
     let panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.display = "block";
     }
   });
 }
